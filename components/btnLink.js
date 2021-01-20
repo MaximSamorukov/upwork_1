c60dcd61f87ea9c04067e7f0f37c62deaa222a18
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-01-20 21:24:30 +0300

    added icons and pseudo elements

--- components/btnLink.js
+++ components/btnLink.js
@@ -1,4 +1,19 @@
-const btnLink = () => {
+function btnLink() {
+  function inCallback(e) {
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    const closeElement = document.createElement('div');
+    closeElement.className = 'btnitem-after';
+    this.append(closeElement);
+  }
+
+  function outCallback(e) {
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+    const closeElement = document.querySelector('.btnitem-after');
+    this.removeChild(closeElement);
+  }
+
   const itemsContainer = document.createElement('div');
   itemsContainer.className = 'items-container';
   const element = document.createElement('div');
@@ -6,6 +21,8 @@ const btnLink = () => {
   Array.from({ length: 20 }).map((i) => {
     const item = document.createElement('div');
     item.className = 'btnitem';
+    item.addEventListener('mouseenter', inCallback);
+    item.addEventListener('mouseleave', outCallback);
     itemsContainer.append(item);
   })
   element.append(itemsContainer);
--- components/social.js
+++ components/social.js
@@ -1,6 +1,15 @@
 const social = () => {
   const element = document.createElement('div');
-  element.textContent = 'social';
   element.className = 'social';
+  const elementInnerContainer = document.createElement('div');
+  elementInnerContainer.className = 'social-container';
+  element.append(elementInnerContainer);
+
+  Array.from({ length: 12 }).map((i, index) => {
+    const item = document.createElement('div');
+    item.className = 'social-item';
+    item.style.backgroundImage = `url(http://newtab.club/img/newtabclub-buttons-${index + 1}.png)`;
+    elementInnerContainer.append(item);
+  })
   return element;
 }
