c3973afb0080a2133e44f730fdfdfab76c79e127
Maxim Samorukov <maxim.samorukov@gmail.com>
2021-01-20 22:45:44 +0300

    added images

--- components/btnLink.js
+++ components/btnLink.js
@@ -17,15 +17,34 @@ function btnLink() {
   const itemsContainer = document.createElement('div');
   itemsContainer.className = 'items-container';
   const element = document.createElement('div');
-  element.className = 'btnlink'
-  Array.from({ length: 20 }).map((i, index) => {
+  element.className = 'btnlink';
+  // const s = {};
+  // const s = Array.from({ length: 200 }).map((i, index) => {
+  //   const img = `http://newtab.club/img/Newtabclub-site-${index + 1}.jpg`;
+  //   const icon = `http://newtab.club/img/Newtabclub-favicon-${index + 1}.jpg`;
+
+  //   return {
+  //     img,
+  //     icon
+  //   };
+  // });
+  // console.log(s);
+  Array.from({ length: 36 }).map((i, index) => {
     const imgContainer = document.createElement('div');
     imgContainer.className = 'image-container';
     const img = document.createElement('img');
     img.src = `http://newtab.club/img/Newtabclub-site-${index + 1}.jpg`;
     imgContainer.append(img);
+
+    const iconContainer = document.createElement('div');
+    iconContainer.className = 'icon-container';
+    const icon = document.createElement('img');
+    icon.src = `http://newtab.club/img/Newtabclub-favicon-${index + 1}.jpg`;
+    iconContainer.append(icon);
+
     const item = document.createElement('div');
     item.append(imgContainer);
+    item.append(iconContainer);
     item.className = 'btnitem';
     item.addEventListener('mouseenter', inCallback);
     item.addEventListener('mouseleave', outCallback);
